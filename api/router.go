963cb82435b897862499f560c096d86ac38b967f
Bevis <>
2024-05-22 11:01:33 +0800

    update

---
+++ api/router.go
@@ -0,0 +1,9 @@
+package api
+
+import (
+	"net/http"
+)
+
+func Listen(w http.ResponseWriter, r *http.Request) {
+	// serve http request
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gin-demo
+
+go 1.21.3
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1

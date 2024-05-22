7995f1fe951c375db6f76861da43299c2f36fc51
Bevis <>
2024-05-22 14:21:22 +0800

    update

---
+++ bot/bot.go
@@ -0,0 +1,34 @@
+package bot
+
+import (
+	"log"
+
+	tgBotApi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func Handler() {
+	bot, err := tgBotApi.NewBotAPI("6706237172:AAFZyrXsYjMg2ion8MH2rG99Pf-Cjf-DjVw")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgBotApi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgBotApi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
+}

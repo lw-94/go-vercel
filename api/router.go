00495b08237513d28d57d9d981a24739c2d3e087
Bevis <>
2024-05-22 11:32:05 +0800

    update

--- api/router.go
+++ api/router.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func Listen(w http.ResponseWriter, r *http.Request) {
 	// serve http request
+	fmt.Printf("request: %s\n", r.URL.Path)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
 }
